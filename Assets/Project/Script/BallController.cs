93823b38ec45e3b672bb7b431cbdfe8595f7505b
josuiyama <josuiyama@gmail.com>
2020-04-25 22:25:32 +0900

    鎖画像追加　鎖動かすスクリプトをコピーしてきた

---
+++ Assets/Project/Script/BallController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BallController : MonoBehaviour
+{
+	Vector3 prev;
+	void Update()
+	{
+		Vector3 p = Camera.main.ScreenPointToRay(Input.mousePosition).origin;
+		p.z = 0;
+
+		if (Input.GetMouseButton(0))
+		{
+			GetComponent<Rigidbody2D>().velocity = (p - this.prev) * 80;
+		}
+		this.prev = p;
+	}
+}
